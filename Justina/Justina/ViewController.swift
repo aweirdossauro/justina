0e9f26605916fb6cb54b92f0cad749290d55afce
d1l4y <viniciusdilay@protonmail.com>
2020-09-01 10:27:40 -0300

    create xcodeproj

---
+++ Justina/Justina/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Justina
+//
+//  Created by Vinicius Augusto Dilay de Paula on 01/09/20.
+//  Copyright © 2020 aweirdossauro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
