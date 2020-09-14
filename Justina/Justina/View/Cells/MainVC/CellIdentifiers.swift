9f6dc200a25192ae2d5b14b8ab375e6e4507ea3a
d1l4y <viniciusdilay@protonmail.com>
2020-09-14 10:26:06 -0300

    add CellIdentifiers.swift

---
+++ Justina/Justina/View/Cells/MainVC/CellIdentifiers.swift
@@ -0,0 +1,19 @@
+//
+//  CellIdentifiers.swift
+//  Justina
+//
+//  Created by Vinicius Augusto Dilay de Paula on 14/09/20.
+//  Copyright © 2020 aweirdossauro. All rights reserved.
+//
+
+import Foundation
+
+/// identifiers das TableViewCells
+struct CellIdentifiers {
+    static let sectionHeaderTableViewCell = "SectionHeaderTableViewCell"
+    static let processosTableViewCell = "ProcessosTableViewCell"
+    static let addProcessoTableViewCell = "AddProcessoTableViewCell"
+    
+    static let messageSentTableViewCell = "MessageSentTableViewCell"
+    static let justinaMessageTableViewCell = "JustinaMessageTableViewCell"
+}
