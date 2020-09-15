2fcafd47236b27cbdbd9772098f3ac8c0bf7e11d
d1l4y <viniciusdilay@protonmail.com>
2020-09-15 11:37:57 -0300

    add mainVC extension

--- Justina/Justina/ViewController/MainVC.swift
+++ Justina/Justina/ViewController/MainVC.swift
@@ -32,15 +32,31 @@ class MainVC: UIViewController, Storyboarded {
     var backView: PassThroughView?
 
     let notification = UINotificationFeedbackGenerator()
-
+    
+    private let imageView = UIImageView(image: UIImage(systemName: "person.circle.fill"))
+    private struct Const {
+    /// Image height/width for Large NavBar state
+        static let ImageSizeForLargeState: CGFloat = 40
+    /// Margin from right anchor of safe area to right anchor of Image
+        static let ImageRightMargin: CGFloat = 16
+    /// Margin from bottom anchor of NavBar to bottom anchor of Image for Large NavBar state
+        static let ImageBottomMarginForLargeState: CGFloat = 12
+    /// Margin from bottom anchor of NavBar to bottom anchor of Image for Small NavBar state
+        static let ImageBottomMarginForSmallState: CGFloat = 6
+    /// Image height/width for Small NavBar state
+        static let ImageSizeForSmallState: CGFloat = 32
+    /// Height of NavBar for Small state. Usually it's just 44
+        static let NavBarHeightSmallState: CGFloat = 44
+    /// Height of NavBar for Large state. Usually it's just 96.5 but if you have a custom font for the title, please make sure to edit this value since it changes the height for Large state of NavBar
+        static let NavBarHeightLargeState: CGFloat = 96.5
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        self.title = "Meus Processos"
-        self.navigationController?.navigationBar.prefersLargeTitles = true
-        
+
         tableView.dataSource = self
         tableView.delegate = self
+        setupNavBarButtonUI()
     }
 
     override func viewWillLayoutSubviews() {
@@ -123,8 +139,13 @@ extension MainVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return indexPath.row == 2 ? 60 : 100
     }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        guard let height = navigationController?.navigationBar.frame.height else { return }
+        moveAndResizeImage(for: height)
+        
+    }
 }
-
 
 //MARK:- Bottom Sheet Related Code
 extension MainVC: UBottomSheetCoordinatorDelegate{
@@ -156,6 +177,59 @@ extension MainVC: UBottomSheetCoordinatorDelegate{
             break
         }
     }
+}
+
+//MARK:- Nav Bar setup related code
+extension MainVC {
+    private func setupNavBarButtonUI() {
+         navigationController?.navigationBar.prefersLargeTitles = true
+
+         title = "Meus Processos"
+
+         imageView.tintColor = .white
+
+         // Initial setup for image for Large NavBar state since the the screen always has Large NavBar once it gets opened
+         guard let navigationBar = self.navigationController?.navigationBar else { return }
+         navigationBar.addSubview(imageView)
+         imageView.layer.cornerRadius = Const.ImageSizeForLargeState / 2
+         imageView.clipsToBounds = true
+         imageView.translatesAutoresizingMaskIntoConstraints = false
+         NSLayoutConstraint.activate([
+             imageView.rightAnchor.constraint(equalTo: navigationBar.rightAnchor, constant: -Const.ImageRightMargin),
+             imageView.bottomAnchor.constraint(equalTo: navigationBar.bottomAnchor, constant: -Const.ImageBottomMarginForLargeState),
+             imageView.heightAnchor.constraint(equalToConstant: Const.ImageSizeForLargeState),
+             imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor)
+             ])
+     }
+     
+     private func moveAndResizeImage(for height: CGFloat) {
+         let coeff: CGFloat = {
+             let delta = height - Const.NavBarHeightSmallState
+             let heightDifferenceBetweenStates = (Const.NavBarHeightLargeState - Const.NavBarHeightSmallState)
+             return delta / heightDifferenceBetweenStates
+         }()
+
+         let factor = Const.ImageSizeForSmallState / Const.ImageSizeForLargeState
+
+         let scale: CGFloat = {
+             let sizeAddendumFactor = coeff * (1.0 - factor)
+             return min(1.0, sizeAddendumFactor + factor)
+         }()
+
+         // Value of difference between icons for large and small states
+         let sizeDiff = Const.ImageSizeForLargeState * (1.0 - factor) // 8.0
+         let yTranslation: CGFloat = {
+             /// This value = 14. It equals to difference of 12 and 6 (bottom margin for large and small states). Also it adds 8.0 (size difference when the image gets smaller size)
+             let maxYTranslation = Const.ImageBottomMarginForLargeState - Const.ImageBottomMarginForSmallState + sizeDiff
+             return max(0, min(maxYTranslation, (maxYTranslation - coeff * (Const.ImageBottomMarginForSmallState + sizeDiff))))
+         }()
+
+         let xTranslation = max(0, sizeDiff - coeff * sizeDiff)
+
+         imageView.transform = CGAffineTransform.identity
+             .scaledBy(x: scale, y: scale)
+             .translatedBy(x: xTranslation, y: yTranslation)
+     }
 }
 
 //MARK:- Coordinator Related Code
