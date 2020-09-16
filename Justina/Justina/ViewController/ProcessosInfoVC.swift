0d4e4beb4adbec430a15427ea7ab8ca1f01e1074
d1l4y <viniciusdilay@protonmail.com>
2020-09-16 11:29:43 -0300

    disable right bar button if bottomSheet is moving

---
+++ Justina/Justina/ViewController/ProcessosInfoVC.swift
@@ -0,0 +1,30 @@
+//
+//  ProcessosInfoVC.swift
+//  Justina
+//
+//  Created by Vinicius Augusto Dilay de Paula on 16/09/20.
+//  Copyright © 2020 aweirdossauro. All rights reserved.
+//
+
+import UIKit
+
+class ProcessosInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
