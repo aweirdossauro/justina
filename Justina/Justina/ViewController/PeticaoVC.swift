3a58d76224a439c5f558b63affba1d70233c8f41
Pyettra Ferreira <pyettraf@gmail.com>
2020-09-21 09:28:51 -0300

    Criação de petição

---
+++ Justina/Justina/ViewController/PeticaoVC.swift
@@ -0,0 +1,23 @@
+//
+//  PeticaoVC.swift
+//  Justina
+//
+//  Created by Pyettra Ferreira on 17/09/20.
+//  Copyright © 2020 aweirdossauro. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PeticaoVC: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+    
+}
