68fddb5868e91e27c41dfc3de83d561434e42719
d1l4y <viniciusdilay@protonmail.com>
2020-10-06 10:26:06 -0300

    rm bottom sheet

--- Justina/Justina/ViewController/MainVC.swift
+++ Justina/Justina/ViewController/MainVC.swift
@@ -56,62 +56,21 @@ class MainVC: UIViewController, Storyboarded {
 
         tableView.dataSource = self
         tableView.delegate = self
+        
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         setupNavBarButtonUI()
+
         showImage(true)
     }
     
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        showImage(false, true)
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        showImage(false)
     }
     
-    
-    override func viewWillLayoutSubviews() {
-        super.viewDidLoad()
-
-        presentSheet()
-    }
-
-    func presentSheet() {
-        guard sheetCoordinator == nil else {return}
-        sheetCoordinator = UBottomSheetCoordinator(parent: self,
-                                                   delegate: self)
-        dataSource = JustinaDataSource()
-        guard let dataSource = dataSource else {
-            print("deu ruim aqui man")
-            return
-        }
-        sheetCoordinator.dataSource = dataSource
-
-        // parentViewController: main view controller that presents the bottom sheet
-        // call this within viewWillLayoutSubViews to make sure view frame has measured correctly. see example projects.
-        let vc = UIStoryboard(name: "Justina", bundle: nil).instantiateViewController(withIdentifier: "JustinaVC") as! JustinaVC
-        
-        vc.sheetCoordinator = sheetCoordinator
-        sheetCoordinator.addSheet(vc, to: self, didContainerCreate: { container in
-            container.roundCorners(corners: [.topLeft, .topRight], radius: 15, rect: container.rect)
-        })
-        sheetCoordinator.setCornerRadius(15)
-
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.01){
-            self.sheetCoordinator.setToNearest(self.sheetCoordinator.availableHeight*0.85, animated: true)
-        }
-    }
-    
-    private func addBackDimmingBackView(below container: UIView){
-        backView = PassThroughView()
-        self.view.insertSubview(backView!, belowSubview: container)
-        backView!.translatesAutoresizingMaskIntoConstraints = false
-        backView!.topAnchor.constraint(equalTo: self.view!.topAnchor).isActive = true
-        backView!.bottomAnchor.constraint(equalTo: container.topAnchor, constant: 10).isActive = true
-        backView!.leadingAnchor.constraint(equalTo: self.view!.leadingAnchor).isActive = true
-        backView!.trailingAnchor.constraint(equalTo: self.view!.trailingAnchor).isActive = true
-    }
     
     @IBAction func testButton(_ sender: Any) {
         delegate?.mainVCDidFinish()
@@ -164,45 +123,6 @@ extension MainVC: UITableViewDelegate, UITableViewDataSource {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         guard let height = navigationController?.navigationBar.frame.height else { return }
         moveAndResizeImage(for: height)
-    }
-}
-
-//MARK:- Bottom Sheet Related Code
-extension MainVC: UBottomSheetCoordinatorDelegate{
-    
-    func bottomSheet(_ container: UIView?, didPresent state: SheetTranslationState) {
-        self.addBackDimmingBackView(below: container!)
-        self.sheetCoordinator.addDropShadowIfNotExist()
-        self.handleState(state)
-    }
-
-    func bottomSheet(_ container: UIView?, didChange state: SheetTranslationState) {
-        handleState(state)
-    }
-
-    func bottomSheet(_ container: UIView?, finishTranslateWith extraAnimation: @escaping ((CGFloat) -> Void) -> Void) {
-        extraAnimation({ percent in
-            self.backView?.backgroundColor = UIColor.black.withAlphaComponent(percent/100 * 0.8)
-        })
-    }
-    
-    func handleState(_ state: SheetTranslationState){
-        switch state {
-        case .progressing(_, let percent):
-            self.backView?.backgroundColor = UIColor.black.withAlphaComponent(percent/100 * 0.8)
-            
-        case .finished(_, let percent):
-            self.backView?.backgroundColor = UIColor.black.withAlphaComponent(percent/100 * 0.8)
-            print(percent,"aqui man")
-            if percent > 95.0 {
-                self.showImage(false, true)
-                break
-            }
-            self.showImage(true, true)
-        default:
-            notification.notificationOccurred(.success)
-            break
-        }
     }
 }
 
@@ -260,15 +180,20 @@ extension MainVC {
         imageView.transform = CGAffineTransform.identity
             .scaledBy(x: scale, y: scale)
             .translatedBy(x: xTranslation, y: yTranslation)
+        imageView.alpha = scale
     }
     
     /// Show or hide the image from NavBar while going to next screen or back to initial screen
     ///
     /// - Parameter show: show or hide the image from NavBar
-    private func showImage(_ show: Bool, _ fast : Bool = false) {
-        let duration = fast ? 0.1 : 0.2
+    private func showImage(_ show: Bool) {
+        let duration = 0.2
         UIView.animate(withDuration: duration) {
             self.imageView.alpha = show ? 1.0 : 0.0
+        }
+        if show {
+            guard let height = navigationController?.navigationBar.frame.height else { return }
+            moveAndResizeImage(for: height)
         }
     }
 }
