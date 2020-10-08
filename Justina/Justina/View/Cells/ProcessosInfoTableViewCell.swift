2be04afc9f4ab2f6affd519e8d0461049343bf6b
d1l4y <viniciusdilay@protonmail.com>
2020-10-08 10:32:17 -0300

    add processos Info table View Cell

---
+++ Justina/Justina/View/Cells/ProcessosInfoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProcessosInfoTableViewCell.swift
+//  Justina
+//
+//  Created by Vinicius Augusto Dilay de Paula on 08/10/20.
+//  Copyright © 2020 aweirdossauro. All rights reserved.
+//
+
+import UIKit
+
+class ProcessosInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
