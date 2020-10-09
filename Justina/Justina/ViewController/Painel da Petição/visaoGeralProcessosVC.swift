d8316bf72566ea7a1473fb253cd00dd09b03313e
d1l4y <viniciusdilay@protonmail.com>
2020-10-09 10:28:05 -0300

    add painel da peticao related VCs

---
+++ Justina/Justina/ViewController/Painel da Petição/visaoGeralProcessosVC.swift
@@ -0,0 +1,30 @@
+//
+//  visaoGeralProcessosVC.swift
+//  Justina
+//
+//  Created by Vinicius Augusto Dilay de Paula on 09/10/20.
+//  Copyright © 2020 aweirdossauro. All rights reserved.
+//
+
+import UIKit
+
+class visaoGeralProcessosVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
