b37ef0c2e96f99e4244d9fc63381219e4e4a35f3
d1l4y <viniciusdilay@protonmail.com>
2020-10-16 09:34:58 -0300

    add tableView config on PeticaoProcessosVC

--- Justina/Justina/ViewController/Painel da Petição/peticaoProcessosVC.swift
+++ Justina/Justina/ViewController/Painel da Petição/peticaoProcessosVC.swift
@@ -12,21 +12,26 @@ class PeticaoProcessosVC: UIViewController, Storyboarded {
     // MARK: Coordinator Related Properties
     static var storyboardName: String = "Main"
     
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension PeticaoProcessosVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
 }
