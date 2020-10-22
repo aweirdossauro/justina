aad27e908df9cf01288f9a9dd1301cd3f42bed98
d1l4y <viniciusdilay@protonmail.com>
2020-10-22 10:27:48 -0300

    add IBDesignable to ProgressView

---
+++ Justina/Justina/View/ProgressView.swift
@@ -0,0 +1,20 @@
+//
+//  ProgressView.swift
+//  Justina
+//
+//  Created by Vinicius Augusto Dilay de Paula on 21/10/20.
+//  Copyright © 2020 aweirdossauro. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class ProgressView: UIView {
+
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+
+}
