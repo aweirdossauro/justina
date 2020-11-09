488b99cfbab9b416dfa14913f26e3dfbd5c578e3
d1l4y <viniciusdilay@protonmail.com>
2020-11-09 10:26:30 -0300

    add NovaPeticaoTableViewCell

---
+++ Justina/Justina/View/Cells/NovaPeticaoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NovaPeticaoTableViewCell.swift
+//  Justina
+//
+//  Created by Vinicius Augusto Dilay de Paula on 09/11/20.
+//  Copyright © 2020 aweirdossauro. All rights reserved.
+//
+
+import UIKit
+
+class NovaPeticaoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
