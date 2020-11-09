1c394d8df228085c957f7ee7b2419461a2e1c07e
d1l4y <viniciusdilay@protonmail.com>
2020-11-09 10:04:38 -0300

    create NovaPeticaoVC

---
+++ Justina/Justina/ViewController/Nova Peticao/NovaPeticaoVC.swift
@@ -0,0 +1,30 @@
+//
+//  NovaPeticaoVC.swift
+//  Justina
+//
+//  Created by Vinicius Augusto Dilay de Paula on 09/11/20.
+//  Copyright © 2020 aweirdossauro. All rights reserved.
+//
+
+import UIKit
+
+class NovaPeticaoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
