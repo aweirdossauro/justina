06aa9e9e142cf798b2dae9994bc02bab7ba18020
d1l4y <viniciusdilay@protonmail.com>
2020-11-09 10:31:49 -0300

    update NovaPeticaoModel

---
+++ Justina/Justina/Model/NovaPeticaoModel.swift
@@ -0,0 +1,35 @@
+//
+//  NovaPeticaoModel.swift
+//  Justina
+//
+//  Created by Vinicius Augusto Dilay de Paula on 09/11/20.
+//  Copyright © 2020 aweirdossauro. All rights reserved.
+//
+
+import Foundation
+
+class NovaPeticaoModel {
+    ///Titulo das Celulas
+    static let tableViewCellTitle : [String] = ["Dados do Requerente",
+                                                "Dados do Processado",
+                                                "Argumentação",
+                                                "Provas - opcional",
+                                                "Testemunhas - opcional",
+                                                "Pedidos"]
+    ///Subtitulo das Celulas
+    static let tableViewCellBody : [String] = ["",
+                                               "",
+                                               "",
+                                               "",
+                                               "",
+                                               ""]
+    
+    enum tableViewDataSource: Int {
+        case dadosRequerente = 0,
+             dadosProcessado,
+             argumentacao,
+             provas,
+             testemunhas,
+             pedidos
+    }
+}
