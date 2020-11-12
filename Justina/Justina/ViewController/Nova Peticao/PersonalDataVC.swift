2955071899442f201b7c93a2fc547d9e975eefd9
d1l4y <viniciusdilay@protonmail.com>
2020-11-12 11:09:22 -0300

    add tableView delegate and dataSource on PersonalDataVC

--- Justina/Justina/ViewController/Nova Peticao/PersonalDataVC.swift
+++ Justina/Justina/ViewController/Nova Peticao/PersonalDataVC.swift
@@ -15,13 +15,43 @@ class PersonalDataVC: UIViewController, Storyboarded {
     static var storyboardName: String = "PersonalData"
     var delegate: PersonalDataVCDelegate?
 
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.register(UINib(nibName: CellIdentifiers.textFieldTableViewCell, bundle: nil),
+                               forCellReuseIdentifier: CellIdentifiers.textFieldTableViewCell)
+        }
+    }
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
 
+}
+
+extension PersonalDataVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        PersonalDataModel.tableViewCellTitle.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = PersonalDataModel.tableViewDataSource.init(rawValue: indexPath.row)
+        switch row {
+        case .nome: break
+        case .nacionalidade: break
+        case .estadoCivil: break
+        case .dataDeNascimento: break
+        case .rg: break
+        case .cpf: break
+        default: break
+        }
+    }
+    
+    
 }
 
 protocol PersonalDataVCDelegate: AnyObject {
