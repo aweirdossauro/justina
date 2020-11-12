00c44e5b68e23bb5e263521cf7c2d418cd9c3667
d1l4y <viniciusdilay@protonmail.com>
2020-11-12 09:56:07 -0300

    update petition flow screens and add TextFieldTableViewCell

---
+++ Justina/Justina/View/Cells/Forms/TextFieldTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TextFieldTableViewCell.swift
+//  Justina
+//
+//  Created by Vinicius Augusto Dilay de Paula on 12/11/20.
+//  Copyright © 2020 aweirdossauro. All rights reserved.
+//
+
+import UIKit
+
+class TextFieldTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
