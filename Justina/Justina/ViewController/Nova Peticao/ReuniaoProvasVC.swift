5d58d0bc801df0a09102797109778450344c270c
d1l4y <viniciusdilay@protonmail.com>
2020-11-20 11:06:14 -0300

    add delegate and dataSource on ReuniaoProvasVc tableView

--- Justina/Justina/ViewController/Nova Peticao/ReuniaoProvasVC.swift
+++ Justina/Justina/ViewController/Nova Peticao/ReuniaoProvasVC.swift
@@ -13,23 +13,30 @@ class ReuniaoProvasVC: UIViewController, Storyboarded {
     // MARK: Coordinator Related Properties
     static var storyboardName: String = "PersonalData"
 
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.register(UINib(nibName: CellIdentifiers.provasTableViewCell, bundle: nil),
+                               forCellReuseIdentifier: CellIdentifiers.provasTableViewCell)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension ReuniaoProvasVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
 }
