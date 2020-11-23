11a470d06bd2cbc8ebd3530edad6e56cb2b553e0
d1l4y <viniciusdilay@protonmail.com>
2020-11-23 11:14:00 -0300

    create peticoesModel

---
+++ Justina/Justina/Model/PeticoesModel.swift
@@ -0,0 +1,31 @@
+//
+//  ReuniaoProvasModel.swift
+//  Justina
+//
+//  Created by Vinicius Augusto Dilay de Paula on 23/11/20.
+//  Copyright © 2020 aweirdossauro. All rights reserved.
+//
+
+import Foundation
+import PDFKit
+
+class Peticoes {
+    var peticoes : [Peticao]
+}
+
+class Peticao {
+
+    
+}
+
+class DadosDoUsuario {}
+
+class DadosDoProcessado {}
+
+class DadosTestemunha {}
+
+class Provas {}
+
+class Pedidos {}
+
+
