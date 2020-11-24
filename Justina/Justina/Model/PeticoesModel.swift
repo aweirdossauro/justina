d4fa9d0c07eef8f49d08112df69c7db7a73e6a64
d1l4y <viniciusdilay@protonmail.com>
2020-11-24 10:18:44 -0300

    update peticoesModel

--- Justina/Justina/Model/PeticoesModel.swift
+++ Justina/Justina/Model/PeticoesModel.swift
@@ -14,13 +14,46 @@ class Peticoes {
 }
 
 class Peticao {
-
+    var dadosDoUsuario : DadosDoUsuario
+    var dadosDoProcessado : DadosDoProcessado
+//    var dadosTestemunha: DadosTestemunha
+//    var provas: Provas
+//    var pedidos: Pedidos
     
+    init(<#parameters#>) {
+        <#statements#>
+    }
 }
 
-class DadosDoUsuario {}
 
-class DadosDoProcessado {}
+struct DadosDoUsuario {
+    var nome: String
+    var nacionalidade: String
+    var estadoCivil: String
+    var dataDeNascimento: Date
+    var rg: String
+    var cpf: String
+}
+
+struct DadosDeContato {
+    var cep: String
+    var logradouro: String
+    var numero: String
+    var cidadeEstado: Date
+    var telefone: String
+    var email: String
+}
+
+struct DadosDoProcessado {
+    var nomeDaEmpresa: String
+    var cnpj: String
+    var cep: String
+    var logradouro: String
+    var numero: String
+    var cidadeEstado: String
+    var telefone: String
+    var emailDaEmpresa: String
+}
 
 class DadosTestemunha {}
 
